5655936fa6b1c6820b3e01f59e3da3a06417dcb3
felixNyalenda <felixasejunior@gmail.com>
2025-02-18 02:20:49 +0300

    Write Unit & Integration Tests using Testcontainers

---
+++ catalog-service/src/test/java/com/asejnr/catalog_service/domain/ProductRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.asejnr.catalog_service.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.utility.TestcontainersConfiguration;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest(
+        properties = {
+                "spring.test.database.replace=none",
+                "spring.datasource.url=jdbc:tc:postgresql:16-alpine:///db",
+        }
+)
+//@Import(TestcontainersConfiguration.class)
+@Sql("/test-data.sql")
+class ProductRepositoryTest {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    void shouldGetAllProducts() {
+        List<ProductEntity> products = productRepository.findAll();
+        assertThat(products).hasSize(15);
+    }
+}
---
+++ catalog-service/src/test/java/com/asejnr/catalog_service/web/controllers/ProductControllerTest.java
@@ -0,0 +1,30 @@
+package com.asejnr.catalog_service.web.controllers;
+
+import com.asejnr.catalog_service.AbstractIntegrationTest;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.jdbc.Sql;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@Sql("/test-data.sql")
+class ProductControllerTest extends AbstractIntegrationTest {
+    @Test
+    void shouldReturnProducts() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .get("/api/products")
+                .then()
+                .statusCode(200)
+                .body("data", hasSize(10))
+                .body("totalElements", is(15))
+                .body("pageNumber", is(1))
+                .body("totalPages", is(2))
+                .body("isFirst", is(true))
+                .body("isLast", is(false))
+                .body("hasNext", is(true))
+                .body("hasPrevious", is(false));
+    }
+}
