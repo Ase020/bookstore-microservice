6738b2364651e04bd517386cfbc8bd7f91f501bf
felixNyalenda <felixasejunior@gmail.com>
2025-02-27 17:39:41 +0300

    Create a Scheduled Job to publish order events to RabbitMQ

--- order-service/src/main/java/com/asejnr/order_service/domain/OrderEventService.java
+++ order-service/src/main/java/com/asejnr/order_service/domain/OrderEventService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Service
 @Transactional
-class OrderEventService {
+public class OrderEventService {
     private static final Logger log = LoggerFactory.getLogger(OrderEventService.class);
 
     private final OrderEventRepository orderEventRepository;
---
+++ order-service/src/main/java/com/asejnr/order_service/jobs/OrderEventsPublishingJob.java
@@ -0,0 +1,26 @@
+package com.asejnr.order_service.jobs;
+
+import com.asejnr.order_service.domain.OrderEventService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+class OrderEventsPublishingJob {
+    private static final Logger log = LoggerFactory.getLogger(OrderEventsPublishingJob.class);
+
+    private final OrderEventService orderEventService;
+
+    OrderEventsPublishingJob(OrderEventService orderEventService) {
+        this.orderEventService = orderEventService;
+    }
+
+    @Scheduled(cron = "${orders.publish-order-events-cron-job}")
+    public void publishOrderEvents() {
+        log.info("Publishing order events at {}", Instant.now().toString());
+        orderEventService.publishOrderEvents();
+    }
+}
