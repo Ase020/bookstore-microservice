e14075352af99ee67029f2a620250865a2b27353
felixNyalenda <felixasejunior@gmail.com>
2025-03-09 22:30:38 +0300

    Monitoring promtail, prometheus, loki, and grafana

--- catalog-service/src/main/java/com/asejnr/catalog_service/web/controllers/ProductController.java
+++ catalog-service/src/main/java/com/asejnr/catalog_service/web/controllers/ProductController.java
@@ -4,6 +4,8 @@ import com.asejnr.catalog_service.domain.PagedResult;
 import com.asejnr.catalog_service.domain.Product;
 import com.asejnr.catalog_service.domain.ProductNotFoundException;
 import com.asejnr.catalog_service.domain.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/products")
 class ProductController {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -19,12 +22,14 @@ class ProductController {
 
     @GetMapping
     PagedResult<Product> getProducts(@RequestParam(name = "page", defaultValue = "1") int page) {
+        log.info("Retrieving products for page: {}", page);
         return productService.getProducts(page);
     }
 
     @GetMapping("/{code}")
     ResponseEntity<Product> getProductByCode(@PathVariable String code) {
         //        sleep();
+        log.info("Retrieving product by code: {}", code);
         return productService
                 .findByCode(code)
                 .map(ResponseEntity::ok)
